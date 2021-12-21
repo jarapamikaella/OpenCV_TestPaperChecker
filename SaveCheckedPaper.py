9df4b61e7b884e6716d9979c151d60b15afb758a
mika <mikaellajarapa@gmail.com>
2021-12-21 18:21:33 +0800

    update window name

--- SaveCheckedPaper.py
+++ SaveCheckedPaper.py
@@ -172,7 +172,7 @@ class Ui_Dialog(object):
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        Dialog.setWindowTitle(_translate("Dialog", "Test Paper Saved"))
         self.label.setText(_translate("Dialog", "Checked Paper saved!"))
 
 
